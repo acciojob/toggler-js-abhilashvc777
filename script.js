3769e7af3232c1af294652365a0f2c3d5a9fb783
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 21:28:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 //your JS code here. If required.
+// const div = document.querySelector(".toggle-container")
+// const input1 = document.querySelector("#good")
+// const input2 = document.querySelector("#cheap")
+// const input3 = document.querySelector("#fast")
+
+// div.addEventListener("click",()=>{
+// 	input1.disabled = false
+// 	input2.disabled = false
+// 	input3.disabled = false
+// 	if(input1.checked && input2.checked){ 
+// 		input3.disabled = true
+// 	}
+// 	if(input1.checked && input3.checked){
+// 		input2.disabled = true
+// 	}
+// 	if(input2.checked && input3.checked){
+// 		input1.disabled = true
+// 	}
+// })
 const div = document.querySelector(".toggle-container")
 const input1 = document.querySelector("#good")
 const input2 = document.querySelector("#cheap")
 const input3 = document.querySelector("#fast")
 
 div.addEventListener("click",(e)=>{
-	input1.disabled = false
-	input2.disabled = false
-	input3.disabled = false
-	if(input1.checked && input2.checked){
-		input3.disabled = true
+	if(input1.checked && input2.checked && !input3.disabled){ 
+		input3.checked = false;
 	}
-	if(input1.checked && input3.checked){
-		input2.disabled = true
+	else if(input1.checked && input3.checked && !input2.disabled){
+		input2.checked = false;
 	}
-	if(input2.checked && input3.checked){
-		input1.disabled = true
+	else if(input2.checked && input3.checked && !input1.disabled){
+		input1.checked = false;
 	}
 })
