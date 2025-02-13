e4b8390f4230e6458177455c2557ec18f0be9852
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 22:46:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //your JS code here. If required.
-const div = document.querySelector(".toggle-container")
-const input1 = document.querySelector("#good")
-const input2 = document.querySelector("#cheap")
-const input3 = document.querySelector("#fast")
+// const div = document.querySelector(".toggle-container")
+// const input1 = document.querySelector("#good")
+// const input2 = document.querySelector("#cheap")
+// const input3 = document.querySelector("#fast")
 
-div.addEventListener("click",()=>{ 
-	input1.disabled = false
-	input2.disabled = false
-	input3.disabled = false
-	if(input1.checked && input2.checked){ 
-		input3.disabled = true
-	}
-	if(input1.checked && input3.checked){
-		input2.disabled = true
-	}
-	if(input2.checked && input3.checked){
-		input1.disabled = true
-	}
-})
+// div.addEventListener("click",()=>{ 
+// 	input1.disabled = false
+// 	input2.disabled = false
+// 	input3.disabled = false
+// 	if(input1.checked && input2.checked){ 
+// 		input3.disabled = true
+// 	}
+// 	if(input1.checked && input3.checked){
+// 		input2.disabled = true
+// 	}
+// 	if(input2.checked && input3.checked){
+// 		input1.disabled = true
+// 	}
+// })
 
 // const div = document.querySelector(".toggle-container")
 // const input1 = document.querySelector("#good")
@@ -35,3 +35,25 @@ div.addEventListener("click",()=>{
 // 		input1.checked = false;
 // 	}
 // })
+
+const div = document.querySelector(".toggle-container")
+const input1 = document.querySelector("#good")
+const input2 = document.querySelector("#cheap")
+const input3 = document.querySelector("#fast")
+
+let checkedBoxes = [];
+
+div.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.type === "checkbox") {
+    if (target.checked) {
+      checkedBoxes.push(target);
+      if (checkedBoxes.length > 2) {
+        checkedBoxes[0].checked = false;
+        checkedBoxes.shift();
+      }
+    } else {
+      checkedBoxes = checkedBoxes.filter(box => box !== target);
+    }
+  }
+});
