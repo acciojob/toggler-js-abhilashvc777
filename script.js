498f4634f6ffbe7f12b0e7ad7a9d4b9e4f3622f0
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 20:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const div = document.querySelector(".toggle-container")
+const input1 = document.querySelector("#good")
+const input2 = document.querySelector("#cheap")
+const input3 = document.querySelector("#fast")
+div.addEventListener("click",(e)=>{
+	// input1.checked = true;
+	console.log()
+})
