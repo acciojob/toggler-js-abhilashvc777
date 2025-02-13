1398f5d77b06d75c4d137b90669a8b2fd8a38232
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 20:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const div = document.querySelector(".toggle-container")
 const input1 = document.querySelector("#good")
 const input2 = document.querySelector("#cheap")
 const input3 = document.querySelector("#fast")
+
 div.addEventListener("click",(e)=>{
-	// input1.checked = true;
-	console.log()
+	input1.disabled = false
+	input2.disabled = false
+	input3.disabled = false
+	if(input1.checked && input2.checked){
+		input3.disabled = true
+	}
+	if(input1.checked && input3.checked){
+		input2.disabled = true
+	}
+	if(input2.checked && input3.checked){
+		input1.disabled = true
+	}
 })
