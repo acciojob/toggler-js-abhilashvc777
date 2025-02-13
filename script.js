fe9c223f3e64f0f860fb9f186c87f0805de922bb
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-13 21:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const input2 = document.querySelector("#cheap")
 const input3 = document.querySelector("#fast")
 
 div.addEventListener("click",(e)=>{
-	input1.disabled = true
-	input2.disabled = true
-	input3.disabled = true
+	input1.disabled = false
+	input2.disabled = false
+	input3.disabled = false
 	if(input1.checked && input2.checked){
-		input3.disabled = false
+		input3.disabled = true
 	}
 	if(input1.checked && input3.checked){
-		input2.disabled = false
+		input2.disabled = true
 	}
 	if(input2.checked && input3.checked){
-		input1.disabled = false
+		input1.disabled = true
 	}
 })
